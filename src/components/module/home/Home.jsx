6f60fdccdcd7cbd3000b2d629706c97265ed640d
Gefyaqiilah <gefyaqiilah26@gmail.com>
2021-02-22 15:08:56 +0700

    init page home

---
+++ src/components/module/home/Home.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import {useSelector} from 'react-redux'
+
+function Home () {
+  
+  return (
+    <div>
+      <h1>ini home</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import HomeComponent from '../components/module/home/Home'
+
+function HomePage () {
+  return (
+    <HomeComponent/>
+  )
+}
+
+export default HomePage
